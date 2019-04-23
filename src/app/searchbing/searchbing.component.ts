5eb3d097d09c15c75f8de05bf1ae1cf072491e3b
dilipkumar <dilipkumar.selvam@csscorp.com>
2019-04-23 14:36:49 +0530

    search results finished

--- src/app/searchbing/searchbing.component.ts
+++ src/app/searchbing/searchbing.component.ts
@@ -3,6 +3,7 @@ import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http
 import { Observable, of } from 'rxjs';
 import { map, catchError, tap } from 'rxjs/operators';
 import { Router } from '@angular/router';
+
 // import { Ngx}
 let endpoint = 'http://localhost:7733';
 
@@ -15,6 +16,27 @@ const httpOptions = {
     'Ocp-Apim-Subscription-Key': subscriptionKey
   })
 };
+/// azure options
+const azureUrlOptions = {
+  apiKey: 'B23575A30B7513505B299CE460DC999D',
+  qs: {
+    apiversion: '2017-11-11',
+    searchfields: 'name'
+  }
+}
+
+const azureOptions = {
+  endpoint: "https://arubacontentsearch.search.windows.net/indexes/azureblob-index6/docs?api-version="
+    + azureUrlOptions.qs.apiversion +"&search=aruba&searchfields=" + azureUrlOptions.qs.searchfields
+}
+// 
+const azureHttpOptions = {
+  headers: new HttpHeaders({
+    'Content-Type': 'application/json',
+    'api-key': azureUrlOptions.apiKey
+  })
+};
+//
 
 @Component({
   selector: 'app-searchbing',
@@ -23,15 +45,19 @@ const httpOptions = {
 })
 export class SearchbingComponent implements OnInit {
   bingResults: any[] = [];
-  options = [{id:1,name:'test1'},{id:2,name:'test2'},{id:3,name:'test3'},
-  {id:4,name:'test'}];
+  azureResults: any[] = [];
+  finalSearchResults: any[] = [];
+  finalSearchResults1: any[] = [];
+  options = [{ id: 1, name: 'aruba' }, { id: 2, name: 'air wave' }, { id: 3, name: 'documentation' },
+  { id: 4, name: 'community' }];
   filteredOptions = [];
   someInput = '';
   pageLength = false;
+  searchTermFinal: string;
 
   constructor(private http: HttpClient, private router: Router) {
-    
-   }
+
+  }
   private extractData(res: Response) {
     let body = res;
     return body || {};
@@ -39,40 +65,71 @@ export class SearchbingComponent implements OnInit {
   /***
    * Filter data based on input text
    */
-  changeInput(){
-    if(this.someInput){
+  changeInput() {
+    if (this.someInput) {
       this.filteredOptions = this.getSearchedValue(this.someInput);
 
     }
   }
-  getSearchedValue(name){
+  getSearchedValue(name) {
     const filterValue = name.toLowerCase();
     return this.options.filter(option => option.name.toLowerCase().indexOf(filterValue) === 0);
   }
+  readAzureData() {
+    
+  }
   readData(data) {
-    console.log("search dta", data);
+    this.bingResults = [];
+    this.azureResults = [];
+    this.finalSearchResults = [];
+    this.finalSearchResults1 = [];
+    // console.log("search dta", data);
     let someInput: String = ""
     let searchTerm = data;
+    this.searchTermFinal = data;
 
 
     let qs = {
       customconfig: customConfigId,
       q: searchTerm
     }
+   
     endpoint = "https://api.cognitive.microsoft.com/bingcustomsearch/v7.0/search?customconfig=" + qs.customconfig + "&q=" + qs.q + "&count=50&offset=0";
     this.listNotification().subscribe((data: any) => {
       // let showData = data
 
-      console.log("printing listData", data.webPages.value);
+      // console.log("printing listData", data.webPages.value);
       this.bingResults = data.webPages.value;
-      if(this.bingResults.length > 0){
-        this.pageLength = true;
-      }
+      console.log(this.bingResults.length, " Bing results found")
+      this.bingResults.forEach(bingObject => {
+        bingObject['isLocked'] = false;
+      });
+      // this.finalSearchResults = this.finalSearchResults.concat(this.bingResults)
+      this.listAzureNotification().subscribe((data: any) => {
+        this.azureResults = data;
+        console.log(this.azureResults.length, " Azure results found")
+        this.finalSearchResults1 = this.finalSearchResults1.concat(this.bingResults)
+        this.finalSearchResults1 = this.finalSearchResults1.concat(this.azureResults);
+        // console.log(this.finalSearchResults1.length)
+        if (this.finalSearchResults1.length > 0) {
+           this.pageLength = true;
+        }
+      });
+      
+      
     });
 
   }
 
   ngOnInit() {
+  }
+
+  accessDocument(event, item) {
+    if (item.isLocked) {
+      alert("You don't have access to view this document")
+    } else {
+      window.open(item.url, "_blank");
+    }
   }
 
   // private _filter(value: string): string[] {
@@ -83,6 +140,17 @@ export class SearchbingComponent implements OnInit {
 
   listNotification(): Observable<any> {
     return this.http.get(endpoint, httpOptions).pipe(
+      map(this.extractData));
+  }
+  listAzureNotification(): Observable<any> {
+    let customEndPoint = "http://localhost:7789/azure?searchTerm="+this.searchTermFinal
+    const customhttpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type':'application/json',
+        'Access-Control-Allow-Origin':'http://localhost:7789'
+      })
+    };
+    return this.http.get(customEndPoint,customhttpOptions).pipe(
       map(this.extractData));
   }
   private handleError<T>(operation = 'operation', result?: T) {
